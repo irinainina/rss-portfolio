a9c5478efdbb3048124729f45795faa79db2e6d0
Ирина <irbris@mail.ru>
2019-08-13 12:15:18 +0300

    Updates

---
+++ precache-manifest.118fb7e4ce13b0d5bf4b9284aacae4d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330fb1993ae48d92da2c5d58dd1e8685",
+    "url": "/rss-portfolio/index.html"
+  },
+  {
+    "revision": "a38c424308d1244e5b96",
+    "url": "/rss-portfolio/static/css/main.d4f2d35c.chunk.css"
+  },
+  {
+    "revision": "817200ea121e02822930",
+    "url": "/rss-portfolio/static/js/2.3696b6ce.chunk.js"
+  },
+  {
+    "revision": "a38c424308d1244e5b96",
+    "url": "/rss-portfolio/static/js/main.bbaeddce.chunk.js"
+  },
+  {
+    "revision": "0d697f2ef08bb9b63603",
+    "url": "/rss-portfolio/static/js/runtime~main.2936a8dc.js"
+  },
+  {
+    "revision": "1cc56b4b124fa66397020a1f0f8ea88d",
+    "url": "/rss-portfolio/static/media/0.1cc56b4b.jpg"
+  },
+  {
+    "revision": "9c491c7c52b11f194475726df494fc02",
+    "url": "/rss-portfolio/static/media/1.9c491c7c.jpg"
+  },
+  {
+    "revision": "04a29dc384db82f9b3001685b3dab8a0",
+    "url": "/rss-portfolio/static/media/2.04a29dc3.jpg"
+  },
+  {
+    "revision": "52e462d3d6a34b900e01735fbbef1694",
+    "url": "/rss-portfolio/static/media/3.52e462d3.jpg"
+  },
+  {
+    "revision": "e40909a5e5dd91fe03db4b4156c0ff01",
+    "url": "/rss-portfolio/static/media/4.e40909a5.jpg"
+  },
+  {
+    "revision": "65742cfc03d932fff0455f5bfde1aa4f",
+    "url": "/rss-portfolio/static/media/5.65742cfc.jpg"
+  },
+  {
+    "revision": "8e98f5013a39626738627f5e1e113435",
+    "url": "/rss-portfolio/static/media/6.8e98f501.jpg"
+  },
+  {
+    "revision": "0399e6803db88b4a2e27c23d7246db27",
+    "url": "/rss-portfolio/static/media/logo.0399e680.jpg"
+  }
+]);
